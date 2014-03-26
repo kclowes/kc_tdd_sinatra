796c243721b6e2cca18adc501611e3c0388a60c8
Keri Clowes <kerilyn.clowes@gmail.com>
2014-03-26 17:12:29 -0600

    User can enter new tasks and they show up in the home page

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 
 class App < Sinatra::Application
-
+  ARR = []
   get '/' do
-    erb :index
+    erb :index, :locals => {:tasks => ARR}
   end
+
+  get '/tasks/new' do
+    erb :new_tasks
+  end
+
+  post '/tasks/new' do
+    ARR << params[:tasks]
+    redirect '/'
+  end
+
+  #get '/' do
+  #  erb :index
+  #end
 end
--- spec/page_spec.rb
+++ spec/page_spec.rb
@@ -8,5 +8,9 @@ feature 'manage items' do
   scenario 'user visits a webpage that displays welcome' do
     visit('/')
     expect(page).to have_content('Welcome')
+    click_link('Add a Task')
+    fill_in('tasks', :with => 'clean')
+    click_on('Create task')
+    expect(page).to have_content('clean')
   end
 end
